5bdff4b6362f1ca5a86c1c3829bdfcdf93eeca24
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2012-06-22 16:44:10 -0400

    small edits to readability

--- video_to_gif.sh
+++ video_to_gif.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo Making a gif from time $1 for $2 seconds from video file $3 to output $4
 VARPATH="global variable"
-function path_find {
-VARPATH=`pwd $3`
-echo $VARPATH
+function __path_find {
+	VARPATH=`pwd $3`
+	echo $VARPATH
 }
-path_find
+__path_find
 mkdir video_to_gif
 cd video_to_gif
 mplayer -ss $1 -endpos $2 -vo png $VARPATH/$3
