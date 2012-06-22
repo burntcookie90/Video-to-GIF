260822e8ccb9c0cb08d753cf6409e2e6bde8acaf
Vishnu Rajeevan <vishnu@localhost.localdomain>
2012-06-22 14:06:36 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Converts a video file to a gif.
+
+Requires mplayer and imagemagick
+
+Usage Example
+	./video_to_gif.sh <startpos> <clip-length> <video-file> <gif-file-name>
+	./video_to_gif.sh 12:55 2 Video.avi output
---
+++ video_to_gif.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo Making a gif from time $1 for $2 seconds from video file $3 to output $4
+VARPATH="global variable"
+function path_find {
+VARPATH=`pwd $3`
+echo $VARPATH
+}
+path_find
+mkdir video_to_gif
+cd video_to_gif
+mplayer -ss $1 -endpos $2 -vo png $VARPATH/$3
+convert *.png $4.gif
+rm *.png
+cd ..
+cp video_to_gif/$4.gif .
+rm -rf video_to_gif
+#echo $VARPATH
